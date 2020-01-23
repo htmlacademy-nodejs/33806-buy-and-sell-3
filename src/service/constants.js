7d9cef8b10858c530dcfc906489332f13710bd91
redVi <liberty195@gmail.com>
2020-01-23 23:51:18 +0700

    move sime data to a separate directory

--- src/service/constants.js
+++ src/service/constants.js
@@ -2,13 +2,32 @@
 
 const DEFAULT_COMMAND = `--help`;
 const USER_ARGV_INDEX = 2;
+
 const ExitCode = {
   success: 0,
   fail: 1
 };
 
+const OfferType = {
+  offer: `offer`,
+  sale: `sale`,
+};
+
+const SumRestrict = {
+  min: 1000,
+  max: 100000,
+};
+
+const PictureRestrict = {
+  min: 0,
+  max: 16,
+};
+
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  ExitCode
+  ExitCode,
+  OfferType,
+  SumRestrict,
+  PictureRestrict
 };
